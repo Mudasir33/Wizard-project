3e1bf63ac3886691823e248bbbc488bba16cf6e4
Ribban <doppel2ganger@gmail.com>
2025-12-04 11:55:41 +0100

    Skapat en fungerarnde traps.js

---
+++ Frontend/game/traps.js
@@ -0,0 +1,56 @@
+export class Trap {
+    constructor(x, y, type) {
+        this.x = x;
+        this.y = y;
+
+        this.size     = type.size;
+        this.visibleDist = type.visibleDist;
+        this.triggerDist = type.triggerDist;
+
+        this.sprite = new Image();
+        this.sprite.src = type.texture;
+
+        this.isActive = true;
+        this.isVisible = true;
+    }
+
+    update(player) {
+        if (!this.isActive) return;
+
+        const dx = player.x - this.x;
+        const dy = player.y - this.y;
+        const dist = Math.hypot(dx, dy);
+
+        if (dist < this.triggerDist) {
+            this.isActive = false;
+            return;
+        }
+        if (dist > this.visibleDist) {
+            this.isVisible = false;
+        }
+        else {
+            this.isVisible = true;
+        }
+    }
+
+    draw(context) {
+        if (!this.isActive || !this.isVisible) return;
+
+        context.drawImage(
+            this.sprite,
+            this.x - this.size / 2,
+            this.y - this.size / 2,
+            this.size,
+            this.size
+        );
+    }
+}
+
+export const trap_list = {
+    fire_trap: {
+        texture: "./fireball.gif",
+        size: 40,
+        visibleDist: 150,
+        triggerDist: 20
+    }
+};
