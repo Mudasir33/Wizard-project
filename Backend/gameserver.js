bb35dd76856281d3c4413d3b1f9015ab6d12444a
Ribban <doppel2ganger@gmail.com>
2025-12-12 03:39:13 +0100

    Fixat så online fungerar för uppdaterad kod
    Ändra localhost till din egen IP

--- Backend/gameserver.js
+++ Backend/gameserver.js
@@ -3,71 +3,51 @@ const http = require('http');
 const { connect } = require('http2');
 const { disconnect } = require('process');
 const socketIO = require('socket.io');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-//const io = socketIO(server);
+// const io = socketIO(server);
+
 const tmx = require('tmx-parser');
 const mapCreation = require('./map.js');
 const path = require('path');
 const { log } = require('console');
+
 app.use(express.static('Backend'));
 app.use(express.static(path.join(__dirname, '../Frontend/game')));
 
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+    },
+});
 
-const io =  new Server(server, {
-cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"]
-    }
-}
-);
-
-let x_kordinater = [0, 1, 2,3,4,5,6,7,8,9]
-let y_kordinater = [0, 1, 2,3,4,5,6,7,8,9]
-const colors = ["blue", "red", "green", "yellow", "brown", "white", "black", "purple", "gray", "rainbow"];
+const x_kordinater = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const y_kordinater = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const colors = ['blue', 'red', 'green', 'yellow', 'brown', 'white', 'black', 'purple', 'gray', 'rainbow'];
 
 let sessions = {
-    "Room 1": {
-        id: "Room 1",
-        players: [],},
-
-    "Room 2": {
-        id: "Room 2",
-        players: [],},
-
-    "Room 3": {
-        id: "Room 3",
-        players: [],},
-
-    "Room 4": {
-        id: "Room 4",
-        players: [],},
-
-    "Room 5": {
-        id: "Room 5",
-        players: [],},
-        
-
-
+    'Room 1': { id: 'Room 1', players: [] },
+    'Room 2': { id: 'Room 2', players: [] },
+    'Room 3': { id: 'Room 3', players: [] },
+    'Room 4': { id: 'Room 4', players: [] },
+    'Room 5': { id: 'Room 5', players: [] },
 };
 
-
-function updateSessions(){
-    io.emit("sessions", sessions);
+function updateSessions() {
+    io.emit('sessions', sessions);
 }
+
 ///------------------- MAP LOADING --------------------
 
-
 ///------------------- GAME SERVER --------------------
-
 
 app.use(express.static('Public'));
 
 const players = {};
 const playerInput = {};
-
-
 
 function wallCollision(map2d, player_x, player_y, player_width, player_height) {
     const obj = Map2d.objectLayers[0].obj;
@@ -78,69 +58,64 @@ function wallCollision(map2d, player_x, player_y, player_width, player_height) {
         const width = obj.obj.object[i].width;
         const height = obj.obj.object[i].height;
 
-        if (player_x < x + width &&
+        if (
+            player_x < x + width &&
             player_x + player_width > x &&
             player_y < y + height &&
-            player_y + player_height > y) {
-            console.log("Collision Detected");
+            player_y + player_height > y
+        ) {
+            console.log('Collision Detected');
         }
-
-
     }
-
 }
 
 const MAX_ROOM = 5;
+
 async function startServer() {
-
-
     const Map2d = await mapCreation();
-    obj = Map2d.objectLayers[0]
-    console.log("TEST OBJECT:", Map2d);
+    obj = Map2d.objectLayers[0];
+    console.log('TEST OBJECT:', Map2d);
 
-
-    io.on("connection", socket => {
-        console.log("connected:", socket.id);
-        io.emit("map", Map2d);
-
+    io.on('connection', (socket) => {
+        console.log('connected:', socket.id);
+        io.emit('map', Map2d);
 
         const number = Object.values(players).length + 1;
 
         players[socket.id] = {
-            x: 500 * Math.random(), //random spawn
-            y: 500 * Math.random(), //random spawn
+            x: 500 * Math.random(), // random spawn
+            y: 500 * Math.random(), // random spawn
             health: 100,
             alive: true,
             id: number,
             speed: 100,
-            sequenceNumber: 0
+            sequenceNumber: 0,
         };
+
         playerInput[socket.id] = { dx: 0, dy: 0 };
         console.log(players);
 
         io.emit('updatePlayers', players);
 
-        console.log("server your player nyumber:", number);
-        socket.emit("playerNumber", number);
+        console.log('server your player nyumber:', number);
+        socket.emit('playerNumber', number);
 
-
-        socket.on("uppos", data => {
-            socket.broadcast.emit("playermoved", data);
+        socket.on('uppos', (data) => {
+            socket.broadcast.emit('playermoved', data);
         });
 
-        socket.on("disconnect", (reason) => {
+        socket.on('disconnect', (reason) => {
             console.log(reason);
 
             delete players[socket.id];
             delete playerInput[socket.id];
-            console.log("player disconneted", socket.id)
+            console.log('player disconneted', socket.id);
             const number = Object.values(players).length + 1;
 
             io.emit('updatePlayers', players);
-
         });
 
-        socket.on("keydown", ({keycode, sequenceNumber}) => {
+        socket.on('keydown', ({ keycode, sequenceNumber }) => {
             players[socket.id].sequenceNumber = sequenceNumber;
             if (!playerInput[socket.id]) return;
             if (keycode == 'KeyW') playerInput[socket.id].dy = -1;
@@ -149,111 +124,95 @@ async function startServer() {
             if (keycode == 'KeyD') playerInput[socket.id].dx = 1;
         });
 
-        socket.on("keyup", (key) => {
+        socket.on('keyup', (key) => {
             if (!playerInput[socket.id]) return;
             if (key == 'KeyW' || key == 'KeyS') playerInput[socket.id].dy = 0;
             if (key == 'KeyA' || key == 'KeyD') playerInput[socket.id].dx = 0;
         });
 
+        // ###################SESSION AND ROOM##################################
+        socket.on('join', (p, room) => {
+            console.log('join recavied');
+            let username_taken = false;
 
-//###################SESSION AND ROOM##################################
-socket.on("join", (p, room )=>{
-    console.log("join recavied");
-    let username_taken = false;
-  
-    if( sessions[room].players.length >= 10){ //is room full
-        console.log("SERVER: try to join full room ")
-        socket.emit("joinerror", "ROOM FULL")
-        return;
-        }
-
-    for ( let i = 0; i < sessions[room].players.length; i++){
-        if(p.username == sessions[room].players[i].username){
-            console.log("JOIN USERNAME JOIN ERROR")
-            username_taken = true; }
-    };
-    
-    if(username_taken == true){
-        socket.emit("joinerror", "USERNAMNE ALREADY TAKEN")
-        return
-    }
-
-    const index =sessions[room].players.length;
-
-    const player ={
-    username: p.username,
-    color: colors[index],
-    ready: false,
-    x: x_kordinater[index],
-    y: y_kordinater[index],
-    health: 100,
-    alive: true,
-      //  this.image = new Image();
-        //this.image.src = 'PixelCharacter.png';
-    speed : 100 }
-
-    
-    sessions[room].players.push(player);
-    socket.emit("joined", room)
-    socket.broadcast.emit("sessions", sessions);
-         });
-
-
- socket.on("update_sessions", ( room)=>{
-        socket.emit("sessions", sessions);
-            });
-
-
-//###############ROOM##################################
-socket.on("ready", (room, p) => {
-    console.log("Server:", p.username, "changing ready")
-    let players = sessions[room].players;
-
-     for ( let i = 0; i < players.length; i++){
-        if(players[i].username === p.username){
-            players[i].ready = p.ready
+            if (sessions[room].players.length >= 10) {
+                // is room full
+                console.log('SERVER: try to join full room ');
+                socket.emit('joinerror', 'ROOM FULL');
+                return;
             }
-      };
-    io.emit("sessions", sessions);
-});
 
-socket.on("room_leave",(room, p)=>{
-    const exroom = sessions[room]
-    console.log("player:",p.username, "leaving room", exroom)
-    for(let i = 0; i < exroom.players.length; i++){
-        if(p.username == exroom.players[i].username ){
-            console.log()
-            sessions[room].players.splice(i, 1);
-            socket.emit("leftroom", sessions)
-            io.emit("sessions", sessions)
-            return;
-            
-        }
-    }
-   
-    
+            for (let i = 0; i < sessions[room].players.length; i++) {
+                if (p.username == sessions[room].players[i].username) {
+                    console.log('JOIN USERNAME JOIN ERROR');
+                    username_taken = true;
+                }
+            }
 
-})
+            if (username_taken == true) {
+                socket.emit('joinerror', 'USERNAMNE ALREADY TAKEN');
+                return;
+            }
 
+            const index = sessions[room].players.length;
 
+            const player = {
+                username: p.username,
+                color: colors[index],
+                ready: false,
+                x: x_kordinater[index],
+                y: y_kordinater[index],
+                health: 100,
+                alive: true,
+                // this.image = new Image();
+                // this.image.src = 'PixelCharacter.png';
+                speed: 100,
+            };
 
+            sessions[room].players.push(player);
+            socket.emit('joined', room);
+            socket.broadcast.emit('sessions', sessions);
+        });
 
+        socket.on('update_sessions', (room) => {
+            socket.emit('sessions', sessions);
+        });
 
-}); //CONNECTION SOCKET
+        // ##############SESSION AND ROOM##################
+        socket.on('ready', (room, p) => {
+            console.log('Server:', p.username, 'changing ready');
+            let players = sessions[room].players;
 
+            for (let i = 0; i < players.length; i++) {
+                if (players[i].username === p.username) {
+                    players[i].ready = p.ready;
+                }
+            }
+            io.emit('sessions', sessions);
+        });
 
-
-
+        socket.on('room_leave', (room, p) => {
+            const exroom = sessions[room];
+            console.log('player:', p.username, 'leaving room', exroom);
+            for (let i = 0; i < exroom.players.length; i++) {
+                if (p.username == exroom.players[i].username) {
+                    console.log();
+                    sessions[room].players.splice(i, 1);
+                    socket.emit('leftroom', sessions);
+                    io.emit('sessions', sessions);
+                    return;
+                }
+            }
+        });
+    }); // CONNECTION SOCKET
 }
-
-
 
 setInterval(() => {
     // Update all player positions based on input
     for (const id in players) {
         const player = players[id];
         const input = playerInput[id];
-        //onsole.log("input:", input);
+        // onsole.log("input:", input);
         if (!input) continue;
 
         let dx = input.dx;
@@ -261,19 +220,18 @@ setInterval(() => {
 
         if (dx !== 0 && dy !== 0) {
             const inv = 1 / Math.sqrt(2);
-            dx *= inv; dy *= inv;
+            dx *= inv;
+            dy *= inv;
         }
 
-        player.x += dx * player.speed * 0.015
+        player.x += dx * player.speed * 0.015;
         player.y += dy * player.speed * 0.015;
     }
     io.emit('updatePlayers', players);
 }, 15);
 
 startServer().catch(console.error);
+
 server.listen(3000, () => {
-    console.log("server start")
-})
-
-
-
+    console.log('server start');
+});
--- Frontend/game/index.js
+++ Frontend/game/index.js
@@ -1,59 +1,53 @@
-import { Player } from "./Player.js";
-const tilesetImage = new Image();
-tilesetImage.src = "/walls_floor.png";
+import { Player } from './Player.js';
 
-const gameCanvas = document.getElementById("canvas");
+const tilesetImage = new Image();
+tilesetImage.src = '/walls_floor.png';
+
+const gameCanvas = document.getElementById('canvas');
 gameCanvas.width = window.innerWidth;
 gameCanvas.height = window.innerHeight;
 
-const canvas = gameCanvas.getContext("2d");
+const canvas = gameCanvas.getContext('2d');
 
 canvas.imageSmoothingEnabled = false;
 let map = null;
 let grid = null;
 
+const socket = io('http://localhost:3000');
 
-const socket = io("http://localhost:3000");
-
-socket.on("connect", () => {
+socket.on('connect', () => {
   console.log(`you connected with id: ${socket.id}`);
 });
 
-socket.on("map", (loadmap) => {
-
-
+socket.on('map', (loadmap) => {
   map = loadmap;
-
-  //console.log("tesst",map.layers[3].length);
-
+  // console.log('tesst', map.layers[3].length);
   requestAnimationFrame(loop);
 });
 
 const frontendPlayers = {};
 
-socket.on("updatePlayers", (backendPlayers) => {
+socket.on('updatePlayers', (backendPlayers) => {
   for (const id in backendPlayers) {
-    const backendPlayer = backendPlayers[id]
+    const backendPlayer = backendPlayers[id];
 
     if (!frontendPlayers[id]) {
       frontendPlayers[id] = new Player(backendPlayer.x, backendPlayer.y);
-    }
-    else {
-        frontendPlayers[id].x = backendPlayer.x;
-        frontendPlayers[id].y = backendPlayer.y;
+    } else {
+      frontendPlayers[id].x = backendPlayer.x;
+      frontendPlayers[id].y = backendPlayer.y;
+
       if (id === socket.id) {
         // Update existing player position
-        const lastBackendInputIndex = playerInputs.findIndex(input => {
+        const lastBackendInputIndex = playerInputs.findIndex((input) => {
           return backendPlayer.sequenceNumber === input.sequenceNumber;
         });
 
-        if (lastBackendInputIndex > -1)
-          playerInputs.splice(0, lastBackendInputIndex + 1);
+        if (lastBackendInputIndex > -1) playerInputs.splice(0, lastBackendInputIndex + 1);
 
         playerInputs.forEach((input) => {
           frontendPlayers[id].x += input.dx;
           frontendPlayers[id].y += input.dy;
-
         });
       } else {
         frontendPlayers[id].x = backendPlayer.x;
@@ -63,17 +57,15 @@ socket.on("updatePlayers", (backendPlayers) => {
           x: backendPlayer.x,
           y: backendPlayer.y,
           duration: 0.015,
-          ease: "linear"
+          ease: 'linear'
         }); */
       }
     }
 
     for (const id in frontendPlayers) {
-      if (!backendPlayers[id]) {
-        delete frontendPlayers[id];
-      }
+      if (!backendPlayers[id]) delete frontendPlayers[id];
     }
-    //console.log(frontendPlayers);
+    // console.log(frontendPlayers);
   }
 });
 
@@ -90,11 +82,10 @@ window.addEventListener('keyup', (e) => {
 const playerInputs = [];
 let sequenceNumber = 0;
 
-
-
 function updatePlayer() {
-  let dx = 0, dy = 0;
-  //console.log("keys:", keys);
+  let dx = 0,
+    dy = 0;
+  // console.log("keys:", keys);
   if (keys['ArrowUp'] || keys['w'] || keys['W']) {
     sequenceNumber++;
     playerInputs.push({ sequenceNumber, dx: 0, dy: -1 });
@@ -120,77 +111,72 @@ function updatePlayer() {
     socket.emit('keydown', { keycode: 'KeyD', sequenceNumber });
   }
 
-
   // Normalize diagonal movement
   if (dx !== 0 && dy !== 0) {
     const inv = 1 / Math.sqrt(2);
-    dx *= inv; dy *= inv;
+    dx *= inv;
+    dy *= inv;
   }
 
   frontendPlayers[socket.id].x += dx * frontendPlayers[socket.id].speed * 0.015;
   frontendPlayers[socket.id].y += dy * frontendPlayers[socket.id].speed * 0.015;
-  
 }
-
-
-
 
 function loop(t) {
   canvas.fillRect(0, 0, canvas.width, canvas.height);
 
   const height = map.layers[0].grid.length;
-  const width = map.layers[0].grid[0].length
+  const width = map.layers[0].grid[0].length;
   const tileWH = 16;
-  const tilesPerRow = (tilesetImage.width / 16);
+  const tilesPerRow = tilesetImage.width / 16;
 
-  //console.log("grid test sx:",  (36 % tilesPerRow)*16); //y
-  //console.log("grid test sy:",  Math.floor(36/tilesPerRow)*16);
-  //console.log("tiledRow: ",tilesetImage.width/16 );
+  // console.log("grid test sx:",  (36 % tilesPerRow)*16); //y
+  // console.log("grid test sy:",  Math.floor(36/tilesPerRow)*16);
+  // console.log("tiledRow: ",tilesetImage.width/16 );
   for (let i = 0; i < map.layers.length; i++) {
-    grid = map.layers[i].grid
+    grid = map.layers[i].grid;
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
-        if (grid[y][x] == null) {
-          continue;
-        }
+        if (grid[y][x] == null) continue;
+
         const { id } = grid[y][x];
 
-        //console.log("id test;", id);  tar ut rätt id (måste va placeringen)
+        // console.log("id test;", id);  tar ut rätt id (måste va placeringen)
         const sx = (id % tilesPerRow) * 16;
         const sy = Math.floor(id / tilesPerRow) * 16;
 
-        //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        // drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
         canvas.drawImage(
           tilesetImage,
           sx,
           sy,
-          tileWH, tileWH,
+          tileWH,
+          tileWH,
           x * tileWH,
           y * tileWH,
-          tileWH, tileWH
-
+          tileWH,
+          tileWH
         );
       }
     }
   }
 
   // tiny red debug square on top (optional, you can remove this)
-  canvas.fillStyle = "#ff0000";
+  canvas.fillStyle = '#ff0000';
   canvas.fillRect(0, 0, 10, 10);
 
   for (const id in frontendPlayers) {
     const player = frontendPlayers[id];
     canvas.drawImage(player.image, player.x, player.y);
-  };
+  }
 
-  //canvas.drawImage(player.image, player.x, player.y);
-  //player.draw();
-  //console.log(player);
+  // canvas.drawImage(player.image, player.x, player.y);
+  // player.draw();
+  // console.log(player);
 }
 
 setInterval(() => {
-  
   updatePlayer();
   requestAnimationFrame(loop);
-  //console.log("inputs:", playerInputs);
+  // console.log("inputs:", playerInputs);
 }, 15);
