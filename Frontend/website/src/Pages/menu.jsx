c3ce70bbb4857ad2738d836c303cdd8d4ee9256b
IMDpanda <160638299+IMDpanda@users.noreply.github.com>
2026-02-20 18:23:22 +0100

    Removed cuztomize

--- Frontend/website/src/Pages/menu.jsx
+++ Frontend/website/src/Pages/menu.jsx
@@ -57,7 +57,6 @@ export default function menu() {
         <div id="buttons">
           <div> <button name="menu"  onClick={() => nav("/sessions")}>Join Lobby</button> </div>
           <div><button name="menu"  onClick={() => nav("/SpectateSession")}>Spectate</button></div>
-          <div><button name="menu"  >Customize</button></div>
           <div><button name="menu" onClick={()=> setGuide(true)} >Guide</button></div>
 
           {isPlaying && (
